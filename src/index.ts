6ee69ea9f52a4849552a4a55c456e4dfdbc84c93
Nikolas Burk <nikolas.burk@gmail.com>
2019-01-11 17:23:49 +0100

     minor adjustments

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import * as express from 'express'
 import * as bodyParser from 'body-parser'
 import { prisma } from './generated/prisma-client'
 
+/**
+ * 
+ * 💡 To explore all available features, fork the CodeSandbox and connect it to your own demo database.
+ * Follow the instructions here: https://github.com/prisma-csb/graphql-example-ts/blob/master/SETUP.md
+ * 
+ */
+
 const app = express()
 
 app.use(bodyParser.json())
