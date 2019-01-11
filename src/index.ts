cb8a4055f58626a1533f1fe60bd8bc1ceaa58649
Nikolas Burk <nikolas.burk@gmail.com>
2019-01-11 17:24:49 +0100

     minor adjustments

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { prisma } from './generated/prisma-client'
 /**
  * 
  * 💡 To explore all available features, fork the CodeSandbox and connect it to your own demo database.
- * Follow the instructions here: https://github.com/prisma-csb/graphql-example-ts/blob/master/SETUP.md
+ * Follow the instructions here: https://github.com/prisma-csb/rest-example-ts/blob/master/SETUP.md
  * 
  */
 
